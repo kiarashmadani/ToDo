5277e90b87fbc69d682eaa5f7a88827818d5fcf1
Kiarash Madani <kiarash.madani85@gmail.com>
2026-09-17 18:43:52 +0330

    style: Added stlesheet to the application

--- ToDoApp/main.cpp
+++ ToDoApp/main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    QFile stylesheet(":/styles.qss");
+    if (stylesheet.open(QFile::ReadOnly | QFile::Text))
+    {
+        QString style = QLatin1String(stylesheet.readAll());
+        a.setStyleSheet(style);
+    }
+
     w.show();
     return QApplication::exec();
 }
